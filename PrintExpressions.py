c2ee245934f4c980ab25082f167aada51e50f97c
Criptyc-ops <cte-jonathan.robinson@sllboces.org>
2025-09-16 13:52:08 +0000

    music

---
+++ PrintExpressions.py
@@ -0,0 +1,13 @@
+'''
+CIS Seniors 25-26
+printing expressions
+'''
+
+print("Hello, my name is Jonathan")
+
+print("867-5309/Jenny")
+print("------------------")
+print("Jenny, Jenny, who can I turn to?")
+print("You give me somethin' I can hold on to")
+print("I know you think I'm like the others before")
+print("Who saw your name and number on the wall")
