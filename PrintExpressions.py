96394eb10db817462ea17a91f80e8d989c8b0f3a
Criptyc-ops <cte-jonathan.robinson@sllboces.org>
2025-09-16 14:48:47 +0000

    More stuff

--- PrintExpressions.py
+++ PrintExpressions.py
@@ -7,7 +7,12 @@ print("Hello, my name is Jonathan")
 
 print("867-5309/Jenny")
 print("------------------")
+print()
 print("Jenny, Jenny, who can I turn to?")
 print("You give me somethin' I can hold on to")
 print("I know you think I'm like the others before")
 print("Who saw your name and number on the wall")
+
+print()
+print(200 + 400)
+print("good morning," + "my name is Jonathan")
