c740ea486f4441ffb8a40af6bc874ad6afab8f2e
Criptyc-ops <cte-jonathan.robinson@sllboces.org>
2025-09-18 13:27:46 +0000

    Stuff

---
+++ dreamVacation.py
@@ -0,0 +1,17 @@
+'''
+dreamVacation.py
+Name: Jonathan Robinson
+course: CIS 25-26
+Date: 2025-09-18
+'''
+
+location = "Bora Bora"
+cost = 29999.99
+duration = 3
+activities = "snorkeling, baha racing, bungie jumping, and skydiving"
+travelCompanion = "Elijah, Queen Elizabeth"
+
+print("My dream vacation would be to " + location + ".")
+print("It would cost around $" + str(cost) + " for a duration of " + str(duration) + " weeks.")
+print("While there, I would like to go " + activities + ".")
+print("I would like to go with " + travelCompanion + ".")
---
+++ morePyVars.py
@@ -0,0 +1,33 @@
+'''
+The doc String
+Notes about python Variables
+Name: Jonathan Robinson
+Date: 2025-09-18
+'''
+print("\n\n\n")
+# Variable Assignment
+firstname = "Jonathan"
+lastname = "Robinson"
+gradeLevel = "Highschool Senior"
+favoriteSubject = "Computer Science"
+yearsAttatchedToThisMortalCoil = 18
+
+print("my name is " + firstname + " " + lastname + ".")
+print("I am a " + gradeLevel + ".")
+print("My favorite subject is " + favoriteSubject + ".")
+print("I have been attatched to this mortal coil for " + str(yearsAttatchedToThisMortalCoil) + " years.")
+print("\n\n\n")
+
+print("mathmatical Calculations:")
+
+#stroring mathematical values
+length = 15
+width = 8
+height = 12
+
+#Calculating the area and volue and store them in variables
+area = length * width
+volume = length * width * height
+
+print("the area of the structure is " + area )
+print("the volume of the structure is " + volume)
