10dc443834cfb40feded98df73d77e341c486660
Criptyc-ops <cte-jonathan.robinson@sllboces.org>
2025-09-18 14:32:26 +0000

    up up down down

---
+++ Unit 1/SocialProfile.py
@@ -0,0 +1,27 @@
+'''
+SocialMediaProfile.py
+name: Jonathan Robinson
+Course: CIS 25-26
+date: 02/18/2025
+'''
+Username = "WhatTheWhat"
+Bio = "Just a dude who does dude things"
+favorite_quote = "loyalty is a two way street, if i ask it from you then your getting it from me -Harvey Specter"
+Followers = 450
+following = 200
+posts = 930
+Average_likes_per_post = 99457.3
+engagement_rate = 2114.76
+verified_account = True
+private_account = False
+
+print("Username:", Username)
+print("Bio:", Bio)
+print("Favorite Quote:", favorite_quote)
+print("Followers:", Followers, "Million")
+print("Following:", following)
+print("Posts:", posts, "thousand")
+print("Average Likes per Post:", Average_likes_per_post)
+print("Engagement Rate:", engagement_rate)
+print("Verified Account:", verified_account)
+print("Private Account:", private_account)
